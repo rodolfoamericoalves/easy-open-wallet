c1d7bd5450da13000ce17e9580cd9dae4e203209
Rodolfo <rodolfoamerico2010@hotmail.com>
2021-06-22 22:33:35 -0300

    initial commit

---
+++ src/test/java/com/easyopenwallet/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyopenwallet.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
